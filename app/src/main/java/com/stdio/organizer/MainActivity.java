2dc30a982e5b37d034d5355a7559b73cf8abf4e2
Std1o <37378410+Std1o@users.noreply.github.com>
2020-05-04 21:20:13 +0300

    edittext dialog

--- app/src/main/java/com/stdio/organizer/MainActivity.java
+++ app/src/main/java/com/stdio/organizer/MainActivity.java
@@ -1,10 +1,14 @@
 package com.stdio.organizer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -38,6 +42,36 @@ public class MainActivity extends AppCompatActivity {
         dataList.add(dataModel);
         dataList.add(dataModel);
         dataList.add(dataModel);
+    }
+
+    public void onClick(View view) {
+        addNote();
+    }
+
+    private void addNote() {
+        View view = getLayoutInflater().inflate(R.layout.add_dialog, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Новое напоминание");
+        final EditText etTitle = view.findViewById(R.id.etTitle);
+        final EditText etDescription = view.findViewById(R.id.etDescription);
+        final DataModel dataModel = new DataModel();
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dataModel.setTitle(etTitle.getText().toString());
+                dataModel.setDescription(etDescription.getText().toString());
+                //myRef.push().setValue(finalDataModel);
+            }
+        });
+
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Отмена", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.setView(view);
+        alertDialog.show();
     }
 
     private void initializeAdapter() {
